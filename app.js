7d128267d2d210ee9ace3cde2165a105fbb31a08
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-12 13:54:15 -0600

    Bcrypt  require
    Forgot to add to past commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 //DB
 const { Db } = require('mongodb');
 const mongoose = require('mongoose');
