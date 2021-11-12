399710085d79c335b278be37484b5d51a2ffd3f4
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-12 13:58:17 -0600

    Changing the mongoose schemas
    Before we had the _id parameter and it was giving us problems remove it and it works :+1:

--- src/models/Comments.js
+++ src/models/Comments.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const comments_schema = new Schema({
-	_id: mongoose.ObjectId,
 	IdPost: mongoose.ObjectId,
 	IdUser: mongoose.ObjectId,
 	Comment: String
