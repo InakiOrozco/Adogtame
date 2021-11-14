8394afc776aa837cf3077d3dbaebd7cfcd79bcc6
IñakiOrozco <greenvana14@gmail.com>
2021-11-13 20:16:41 -0600

    agregando info para trabajar

--- src/models/Groups.js
+++ src/models/Groups.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema
 
 const groups_schema = new Schema({
 	name: String,
-	description: String
+	description: String,
+	group_admin: Array
 }, { timestamps: true });
 
 const groups = mongoose.model('Groups', groups_schema);
