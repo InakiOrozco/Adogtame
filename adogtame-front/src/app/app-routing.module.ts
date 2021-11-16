c8000c866aa7b974ce47e60a8e1510ba57108d6d
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-15 19:46:58 -0600

    Created Angular base app

---
+++ adogtame-front/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+
+const routes: Routes = [
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
