0ae76bbbd8e462943415993450ba548cc61cbacf
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-16 22:48:23 -0600

    Added google passport auth

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const router = express.Router();
 const cors = require('cors')
 //DB
 const mongoose = require('mongoose');
@@ -12,9 +11,13 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 //Routers
+const ApiRouter = express.Router();
 const GroupsRouter = require('./src/routes/Groups.routes');
 const UsersRouter = require('./src/routes/Users.routes');
 const PostsRouter = require('./src/routes/Posts.routes');
+const passport = require('passport')
+ApiRouter.use(passport.initialize());
+const AuthRouter = require('./src/auth');
 
 //Swagger
 const swaggerJsDoc = require('swagger-jsdoc');
@@ -37,26 +40,25 @@ const swaggerOptions = {
 }
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-router.use(express.urlencoded({ extended: true }));
-router.use(express.json());
-router.get('/', (req, res) => { res.send('adogtame api') });
-router.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-router.use('/', GroupsRouter);
-router.use('/', UsersRouter);
-router.use('/', PostsRouter);
-
+ApiRouter.use(express.urlencoded({ extended: true }));
+ApiRouter.use(express.json());
+ApiRouter.get('/', (req, res) => { res.send('adogtame api') });
+ApiRouter.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+ApiRouter.use('/auth', AuthRouter);
+ApiRouter.use(passport.session());
+ApiRouter.use('/', GroupsRouter);
+ApiRouter.use('/', UsersRouter);
+ApiRouter.use('/', PostsRouter);
 
 const uri = "mongodb+srv://" + process.env.DB + "?retryWrites=true&w=majority"
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => {
         console.log("connected to db...")
         app.use(cors());
-        app.use('/api', router);
+        app.use('/api', ApiRouter);
         app.use(express.static(path.join(__dirname, "adogtame-front", "dist", "adogtame-front")));
+
         app.listen(port, () => {
-
-
-
             console.log('App API is listening in port: ' + port);
             console.log('http://localhost:' + port + '/api/');
             console.log('Swagger Docs: ' + 'http://localhost:' + port + '/api/docs');
