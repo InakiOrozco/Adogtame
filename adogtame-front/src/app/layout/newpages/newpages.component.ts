b8c14cacd618f6c6b5991c3ffd79b585e898fc4e
IñakiOrozco <greenvana14@gmail.com>
2021-11-16 21:48:53 -0600

    estructura básica

--- adogtame-front/src/app/layout/newpages/newpages.component.ts
+++ adogtame-front/src/app/layout/newpages/newpages.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Page{
+  title:string;
+}
+
 @Component({
   selector: 'app-newpages',
   templateUrl: './newpages.component.html',
   styleUrls: ['./newpages.component.scss']
 })
 export class NewpagesComponent implements OnInit {
+
+  pages: Array<Page>=[{title: "Guadalajara Norte - Gatos"}, {title: "Guadalajara Sur - Perros"}, {title: "Guadalajara Este - Casas Puente"}, {title: "Guadalajara Oeste - Perros y gatos"}];
 
   constructor() { }
 
