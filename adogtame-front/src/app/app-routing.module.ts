975a91239ade238291705b5ab0366512994adcdb
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-16 20:16:14 -0600

    Simon

--- adogtame-front/src/app/app-routing.module.ts
+++ adogtame-front/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './common/guards/auth.guard';
+import { LoggedGuard } from './common/guards/logged.guard';
 import { CreategroupComponent } from './pages/creategroup/creategroup.component';
 import { CreatepostComponent } from './pages/createpost/createpost.component';
 import { GroupComponent } from './pages/group/group.component';
@@ -11,15 +13,15 @@ import { UserComponent } from './pages/user/user.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 const routes: Routes = [
-  {path:'', component: WelcomeComponent},
-  {path:'login', component: LoginComponent},
-  {path:'register', component: RegisterComponent},
-  {path:'home', component:HomeComponent},
-  {path:'user', component:UserComponent},
-  {path:'group', component:GroupComponent},
-  {path:'creategroup', component:CreategroupComponent},
-  {path:'post', component:PostComponent},
-  {path:'createpost', component:CreatepostComponent}
+  {path:'', component: WelcomeComponent,canActivate: [LoggedGuard]},
+  {path:'login', component: LoginComponent,canActivate: [LoggedGuard]},
+  {path:'register', component: RegisterComponent,canActivate: [LoggedGuard]},
+  {path:'home', component:HomeComponent, canActivate: [AuthGuard]},
+  {path:'user', component:UserComponent, canActivate: [AuthGuard]},
+  {path:'group', component:GroupComponent, canActivate: [AuthGuard]},
+  {path:'creategroup', component:CreategroupComponent, canActivate: [AuthGuard]},
+  {path:'post', component:PostComponent, canActivate: [AuthGuard]},
+  {path:'createpost', component:CreatepostComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
