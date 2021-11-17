584a5674ea2eb0eaa9ec02fdf65781e5b4ced753
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-16 20:12:16 -0600

    Added httplciente for auth service petitions

--- adogtame-front/src/app/app.module.ts
+++ adogtame-front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -45,6 +46,7 @@ import { CreatepostComponent } from './pages/createpost/createpost.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
