60c35ac2d15cdcd8acd26ca8915da3ef7493bdb6
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-24 16:15:26 -0600

    Added functionality to header
    Once you login, the header will display the name of the user, a home button to take you to /home and a menu, in witch you can logout or go to se  your profile, also the name when clicked, sends you to you profile.

--- front-material/src/app/layout/header/header.component.ts
+++ front-material/src/app/layout/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/common/services/auth.service';
+import { CreateGroupDialog } from '../your-pages/your-pages.component';
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
@@ -7,15 +11,30 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  isLoggedIn = true;
+  user$:Observable<any>;
+  user:any = {isLoggedIn:false};
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public authService: AuthService, private dialog: MatDialog) {
+    this.user$ = new Observable<Object>();
+  }
 
   ngOnInit(): void {
+    this.authService.loadUser();
+    this.user$ = this.authService.getUser$();
+    this.user$.subscribe(user => {  
+      if(user.token){
+        this.user = user}else{this.user.isLoggedIn = false}
+    });
+
   }
 
   logOut(){
-    this.router.navigate(['/']);
+    this.authService.logOut();
+    this.user = {};
+  }
+
+  createGroup(){
+    const dialogRef = this.dialog.open(CreateGroupDialog);
   }
 
 }
