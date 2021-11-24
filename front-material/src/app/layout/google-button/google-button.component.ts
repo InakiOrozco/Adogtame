93a54825faff595d9163d7197b4a2ea131a962d9
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-24 16:12:54 -0600

    Added functionality to the sign in with google button
    Make a call to the login auth service to handle signin with google

--- front-material/src/app/layout/google-button/google-button.component.ts
+++ front-material/src/app/layout/google-button/google-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/common/services/auth.service';
 
 @Component({
   selector: 'google-button',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoogleButtonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  onClick(){
+    this.authService.googleAuth();
   }
 
 }
