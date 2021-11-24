0cf1142f4a9189806e50cca55985b9728889d0bf
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-24 16:10:13 -0600

    Added functionality to login component
    Added form handling and calls to the auth service to handle login

--- front-material/src/app/pages/login/login.component.ts
+++ front-material/src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/common/services/auth.service';
 
 @Component({
   selector: 'login',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private authService:AuthService) { 
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required])
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    console.log('here')
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      this.authService.login(this.loginForm.value.email, this.loginForm.value.password);
+    }
+  }
 }
