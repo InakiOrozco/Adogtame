9cbb3a9a5c2307459b9e62bf40a1476e68450768
IñakiOrozco <greenvana14@gmail.com>
2021-11-24 22:52:09 -0600

    falta testear

--- back-end/src/routes/Images.routes.js
+++ back-end/src/routes/Images.routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 const path = require('path');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
 
+aws.config.update({
+	secretAccessKey: process.env.DB_AWS_KEY,
+	accessKeyId: process.env.DB_AWS_KEY,
+	region: 'us-east-1'
+});
+
+/*
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		cb(null, path.join(__dirname, '..', 'public', 'images'));
@@ -11,10 +21,17 @@ const storage = multer.diskStorage({
 		let extension = extArray[extArray.length - 1];
 		cb(null, `${Date.now()}.${extension}`);
 	}
-});
+});*/
 
 const upload = multer({
-	storage,
+    storage: multerS3({
+        s3: s3,
+        bucket: 'adogtame-fotos',
+        key: function (req, file, cb) {
+            console.log(file);
+            cb(null, Date.now()); //file key
+        }
+    }),
 	dest: path.join(__dirname, '..', 'public', 'images'),
 	fileFilter: (req, file, cb) => {
 		if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
