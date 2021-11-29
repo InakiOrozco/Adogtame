f67ba5fb64ed2ff14eb77418b1813e3f6f3dea7d
Gustavo Sotres <gustavonline.games@gmail.com>
2021-11-29 17:52:42 -0600

    Updated user page
    Now it ask the for the user passed on the url

--- front-material/src/app/pages/user/user.component.ts
+++ front-material/src/app/pages/user/user.component.ts
@@ -1,5 +1,8 @@
+import { ifStmt } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UsersService, User } from 'src/app/common/services/users.service';
+
 
 interface Post {
   id_post:string
@@ -21,29 +24,29 @@ interface Post {
 })
 export class UserComponent implements OnInit {
   posts:any = [];
-  user:any = {};
+  user:User | any = {};
 
   
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private usersService: UsersService) { }
   
   
   ngOnInit(): void {
-    this.user.id = this.route.snapshot.paramMap.get('id');
-    this.user.name = "User with id:";
-    this.user.last_name = "" + this.user.id;
-    this.user.full_name = this.user.name + this.user.last_name;
-    this.user.photo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
-    this.user.tags = ['DogCare', 'Veterinarian'];
-    this.posts = [
-      {id_post: '1254', id_group:'78324',id_user: this.user.id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
-      {id_post: '254254', id_group:'7831',id_user: this.user.id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
-      {id_post: '245', id_group:'4387',id_user: this.user.id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
-      {id_post: '254', id_group:'387737457',id_user: this.user.id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
-      {id_post: '254', id_group:'4354',id_user: this.user.id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
-      {id_post: '254254', id_group:'37837',id_user: this.user.id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
-      {id_post: '24257', id_group:'387378387',id_user: this.user.id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
-      {id_post: '7387', id_group:'3878',id_user: this.user.id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false}
-    ]
+    const idUser_URL = this.route.snapshot.paramMap.get('id');
+    if(idUser_URL){
+      this.usersService.getUser(idUser_URL).subscribe(user => {
+        this.user = user;
+        this.posts = [
+          {id_post: '1254', id_group:'78324',id_user: this.user._id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
+          {id_post: '254254', id_group:'7831',id_user: this.user._id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
+          {id_post: '245', id_group:'4387',id_user: this.user._id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
+          {id_post: '254', id_group:'387737457',id_user: this.user._id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
+          {id_post: '254', id_group:'4354',id_user: this.user._id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
+          {id_post: '254254', id_group:'37837',id_user: this.user._id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
+          {id_post: '24257', id_group:'387378387',id_user: this.user._id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false},
+          {id_post: '7387', id_group:'3878',id_user: this.user._id, title:'Titulo 1', information:'Informacion de prueba', photo:'https://dogtime.com/assets/uploads/2011/03/puppy-development.jpg', location: 'Mi casa', contact_info:'test@google.com', pet_type:'Dog', resolved:false}
+        ]
+      });
+    }
   }
 
 }
