920ce8e9cb642296845a8ad19f3e30d4013de8b5
Gustavo Sotres <gustavonline.games@gmail.com>
2021-12-04 18:51:17 -0600

    Created posts service

---
+++ front-material/src/app/common/services/posts.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { apiURL } from '../globals';
+import { AuthService } from './auth.service';
+
+export interface Post {
+  _id: string,
+  id_user: string,
+  title: string,
+  information: string,
+  photo: string,
+  location: string,
+  contact_info: string,
+  pet_type: string,
+  resolverd: boolean
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  httpOptions:any = {};
+
+  constructor(private authService: AuthService, private http:HttpClient) {
+    this.httpOptions = {
+      headers: new HttpHeaders({ 
+        'Access-Control-Allow-Origin':'*',
+        'x-access-token': this.authService.getToken()
+      })
+    };
+  }
+
+  getPostsByUserId(userID: string | any){
+    return this.http.get<Array<Post>>(apiURL + "/users/" + userID + "/posts", this.httpOptions);
+  }
+}
