55cad1cd075bbda84e626d3c81402d75024b3a09
Gustavo Sotres <gustavonline.games@gmail.com>
2021-12-05 21:50:50 -0600

    Update home.component.ts
    Now it gets the post from the back end

--- front-material/src/app/pages/home/home.component.ts
+++ front-material/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService, Post } from 'src/app/common/services/posts.service';
 
 @Component({
   selector: 'home',
@@ -6,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  posts:Array<any> = []
-  constructor() { }
+  posts:Array<Post> | any = [];
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.postsService.getPostFromSubscribedGroups().subscribe(posts => {
+      if(posts){
+        this.posts = posts;
+        this.posts = this.posts[0];
+        console.log(this.posts);
+      }
+    })
   }
   
 }
